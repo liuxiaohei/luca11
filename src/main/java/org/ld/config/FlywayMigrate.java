9cec54be587bf84c71a1f62cf252e1afb0c0eea2
liuxiaohei <2297598383@qq.com>
2020-04-22 14:16:58 +0800

    simplify

--- src/main/java/org/ld/config/FlywayMigrate.java
+++ src/main/java/org/ld/config/FlywayMigrate.java
@@ -8,6 +8,10 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+import java.util.Optional;
 
 /**
  */
@@ -27,13 +31,32 @@ public class FlywayMigrate {
 
     @PostConstruct
     public void init() {
-        Flyway flyway = Flyway.configure().dataSource(jdbcUrl,userName,passWord).load();
+        final String targetDb = Optional.ofNullable(jdbcUrl)
+                .map(str -> str.split("/"))
+                .map(strs -> strs[strs.length - 1])
+                .map(str -> str.split("[?]"))
+                .map(strs -> strs[0]).orElse("");
+        final String rawJdbcUrl = Optional.ofNullable(jdbcUrl).map(e -> e.split(targetDb)[0]).orElse("");
+        final String initSql = "CREATE DATABASE IF NOT EXISTS " + targetDb;
+        final Flyway flyway = Flyway
+                .configure()
+                .dataSource(
+                        Optional.of(jdbcUrl)
+                                .filter(e -> !e.contains("useSSL")).map(e -> e + "&useSSL=false")
+                                .orElse(jdbcUrl)
+                        , userName
+                        , passWord).load();
         log.info("开始迁移数据");
-        try {
+        log.info("init Db jdbcurl:" + rawJdbcUrl + " database:" + targetDb); //自动创建database
+        try (final Connection connection = DriverManager.getConnection(rawJdbcUrl, userName, passWord);
+             final Statement statement = connection.createStatement()) {
+            statement.execute(initSql);
+            flyway.repair();
             flyway.migrate();
             log.info("迁移结束");
         } catch (Exception e) {
-            log.error("",e);
+            log.error("", e);
+            throw new RuntimeException(e);
         }
     }
 
