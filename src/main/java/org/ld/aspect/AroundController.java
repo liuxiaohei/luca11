d91fae1f8a305ac24b51e811d82780aa35c47a52
liuxiaohei <2297598383@qq.com>
2020-04-22 14:08:26 +0800

    simplify

--- src/main/java/org/ld/aspect/AroundController.java
+++ src/main/java/org/ld/aspect/AroundController.java
@@ -3,19 +3,27 @@ package org.ld.aspect;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.ld.utils.ControllerUtil;
+import org.ld.beans.ResponseBodyBean;
+import org.ld.enums.SystemErrorCodeEnum;
+import org.ld.exception.CodeStackException;
+import org.ld.exception.ErrorCode;
 import org.ld.utils.JsonUtil;
 import org.ld.utils.LoggerUtil;
 import org.ld.utils.UuidUtils;
+import org.slf4j.Logger;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Optional;
-import org.slf4j.Logger;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @SuppressWarnings("unused")
 @Aspect
@@ -37,7 +45,47 @@ public class AroundController {
         LOG.info(shortUUid + ":请求IP:" + request.getRemoteAddr());
         LOG.info(shortUUid + ":CLASS_METHOD : " + point.getSignature().getDeclaringTypeName() + "." + point.getSignature().getName());
         LOG.info(shortUUid + ":参数: " + Optional.ofNullable(point.getArgs()).map(Arrays::asList).orElseGet(ArrayList::new).stream().map(JsonUtil::obj2Json).collect(Collectors.joining(",")));
-        final var object = ControllerUtil.convertResponseBody(shortUUid,point::proceed);
+        Object object;
+        try {
+            final var result = new ResponseBodyBean<>();
+            result.setData(point.proceed());
+            result.setErrorCode(0);
+            result.setMessage("成功");
+            LOG.info(shortUUid + ":Response Body : " + JsonUtil.obj2Json(result));
+            object = new ResponseEntity<>(result, HttpStatus.OK);
+        } catch (Throwable e) {
+            LOG.error(shortUUid, e);
+            final var se = Optional.of(e)
+                    .map(t -> {
+                        var t1 = t;
+                        while (null != t1) {
+                            if (t1 instanceof CodeStackException) return (CodeStackException) t1;//找到第一个CodeException
+                            t1 = t1.getCause();
+                        }
+                        return null;
+                    })
+                    .orElseGet(() -> SystemErrorCodeEnum.getSystemError(e));
+            final var result = new ResponseBodyBean<>();
+            result.setErrorCode(Optional.of(se)
+                    .map(CodeStackException::getErrorCode)
+                    .map(ErrorCode::getCode)
+                    .orElseGet(SystemErrorCodeEnum.UNKNOWN::getCode));
+            result.setStackTrace(Optional.of(e)
+                    .map(error -> {
+                        final var sw = new StringWriter();
+                        e.printStackTrace(new PrintWriter(sw));
+                        return Stream.of(sw.toString().split("\n\t"))
+                                .skip(1)
+                                .map(str -> str.replace("\n", ""))
+                                .toArray(String[]::new);
+                    }).orElse(null));
+            result.setMessage(Optional.of(se)
+                    .map(CodeStackException::getErrorCode)
+                    .filter(i -> !i.getCode().equals(SystemErrorCodeEnum.UNKNOWN.getCode()))
+                    .map(ErrorCode::getMessage)
+                    .orElseGet(e::getMessage));
+            object = new ResponseEntity<>(result, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
         final var end = System.currentTimeMillis();
         LOG.info(shortUUid + ":执行时间: " + (end - start) + " ms!");
         return object;
