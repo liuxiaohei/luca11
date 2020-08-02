797bc08c61ef536d71a68823f0f9df678dd97c8d
liuxiaohei <di.liu@transwarp.io>
2020-08-02 18:59:56 +0800

    ChatRoom

--- src/main/java/org/ld/config/LucaConfig.java
+++ src/main/java/org/ld/config/LucaConfig.java
@@ -10,12 +10,13 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.oas.annotations.EnableOpenApi;
-import springfox.documentation.service.Contact;
-import springfox.documentation.service.ResponseMessage;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 
 import java.util.List;
+import java.util.Set;
 import java.util.concurrent.ForkJoinPool;
 
 //import javax.servlet.MultipartConfigElement;
@@ -45,7 +46,9 @@ public class LucaConfig {
 //    }
 
     /**
+     * https://blog.csdn.net/lilinhai548/article/details/107394670
      * http://127.0.0.1:9999/swagger-ui.html#/
+     * http://127.0.0.1:9999/swagger-ui/index.html
      */
     @Bean
     public Docket createRestApi() {
@@ -60,6 +63,24 @@ public class LucaConfig {
                 .apis(RequestHandlerSelectors.basePackage("org.ld.controller"))
                 .paths(PathSelectors.any())
                 .build()
+                // 支持的通讯协议集合
+                .protocols(Set.of("https", "http"))
+                .securitySchemes(List.of(new ApiKey("BASE_TOKEN", "token", "pass")))
+                // 授权信息全局应用
+                .securityContexts(List.of(
+                        SecurityContext
+                                .builder()
+                                .securityReferences(
+                                        List.of(new SecurityReference(
+                                                        "BASE_TOKEN",
+                                                        new AuthorizationScope[]{
+                                                                new AuthorizationScope("global", "")
+                                                        }
+                                                )
+                                        )
+                                )
+                                .build()
+                ))
                 .apiInfo(new ApiInfoBuilder()
                         .title("LUCA")
                         .description("系统介绍")
@@ -67,6 +88,25 @@ public class LucaConfig {
                         .version("1.0")
                         .build());
     }
+
+//    @SuppressWarnings("unchecked")
+//    public void addInterceptors(InterceptorRegistry registry) {
+//        try {
+//            Field registrationsField = ClassUtils.getField(InterceptorRegistry.class, "registrations");
+//            List<InterceptorRegistration> registrations = (List<InterceptorRegistration>) ReflectionUtils.getField(registrationsField, registry);
+//            if (registrations != null) {
+//                for (InterceptorRegistration interceptorRegistration : registrations) {
+//                    interceptorRegistration
+//                            .excludePathPatterns("/swagger**/**")
+//                            .excludePathPatterns("/webjars/**")
+//                            .excludePathPatterns("/v3/**")
+//                            .excludePathPatterns("/doc.html");
+//                }
+//            }
+//        } catch (Exception e) {
+//
+//        }
+//    }
 
     /**
      * https://www.cnblogs.com/javazhiyin/p/9851775.html
