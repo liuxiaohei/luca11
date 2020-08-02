b4cd481d5a3d8450e40c4c599a00b292e52d5f76
liuxiaohei <di.liu@transwarp.io>
2020-08-02 22:44:25 +0800

    ChatRoom

--- src/main/java/org/ld/config/WebConfiguration.java
+++ src/main/java/org/ld/config/WebConfiguration.java
@@ -6,23 +6,27 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 /**
+ * // todo 整理
  * https://www.cnblogs.com/oldboyooxx/p/10824531.html
+ * https://cloud.tencent.com/developer/article/1376509
  * 解决统一包装类返回String 类型报错
  */
 @Configuration
-public class WebConfiguration implements WebMvcConfigurer {
+public class WebConfiguration implements WebFluxConfigurer {
 
     @Value("${spring.jackson.date-format:yyyy-MM-dd HH:mm:ss}")
     private String pattern; // 全局配置 LocalDateTime 的转换方式
 
-    @Override
+    // WebMvcConfigurer
+//    @Override
     public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
         final var builder = Jackson2ObjectMapperBuilder.json();
         builder.serializerByType(LocalDateTime.class, new LocalDateTimeSerializer(DateTimeFormatter.ofPattern(pattern))); // 再包装日期类型的转换
