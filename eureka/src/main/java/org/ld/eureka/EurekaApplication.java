7064bd5a7106e145144628737dd2fd4ee4542d39
liuxiaohei <di.liu@transwarp.io>
2020-09-05 23:24:00 +0800

    aaa

---
+++ eureka/src/main/java/org/ld/eureka/EurekaApplication.java
@@ -0,0 +1,16 @@
+package org.ld.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * ServiceDiscoveryApplication
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
