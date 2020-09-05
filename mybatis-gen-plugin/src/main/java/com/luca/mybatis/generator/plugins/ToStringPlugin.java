e4422e4a8b18dd34cbd68798c287c9754711e4cd
liuxiaohei <di.liu@transwarp.io>
2020-09-06 00:08:09 +0800

    aaa

--- mybatis-gen-plugin/src/main/java/com/luca/mybatis/generator/plugins/ToStringPlugin.java
+++ mybatis-gen-plugin/src/main/java/com/luca/mybatis/generator/plugins/ToStringPlugin.java
@@ -1,16 +1,15 @@
 package com.luca.mybatis.generator.plugins;
 
-import java.util.Iterator;
-import java.util.List;
-
-import org.mybatis.generator.api.PluginAdapter;
 import org.mybatis.generator.api.IntrospectedColumn;
 import org.mybatis.generator.api.IntrospectedTable;
+import org.mybatis.generator.api.PluginAdapter;
 import org.mybatis.generator.api.dom.java.FullyQualifiedJavaType;
 import org.mybatis.generator.api.dom.java.JavaVisibility;
 import org.mybatis.generator.api.dom.java.Method;
 import org.mybatis.generator.api.dom.java.TopLevelClass;
 import org.mybatis.generator.internal.util.JavaBeansUtil;
+
+import java.util.List;
 
 /**
  * Generates a <tt>toString</tt> method that returns a string representation of all fields.
