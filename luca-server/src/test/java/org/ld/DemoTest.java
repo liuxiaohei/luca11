ce6b16f18e0a47e3231fbeb1f2f6979b50808311
liuxiaohei <di.liu@transwarp.io>
2020-09-29 15:09:23 +0800

    aaa

--- luca-server/src/test/java/org/ld/DemoTest.java
+++ luca-server/src/test/java/org/ld/DemoTest.java
@@ -115,6 +115,58 @@ public class DemoTest {
     }
 
 
+    @Test
+    public void tdt1_Xdabao() throws IOException {
+        var tag = "studio-1.5.0-rc1";
+        var list = Arrays.asList("tdt","canal-server","canal-client");
+        var digestMap = new HashMap<String,String>();
+        list.forEach(s -> {
+            Process process;
+            try {
+                System.out.println("docker pull 172.16.1.99/gold/" + s + ":studio-1.5");
+                process = Runtime.getRuntime().exec("docker pull 172.16.1.99/gold/" + s + ":studio-1.5");
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            var result = convertStreamToStr(process.getInputStream());
+            if(result.size() < 2) {
+                throw new RuntimeException("拉取镜像失败");
+            }
+            digestMap.put(s,result.get(1));
+        });
+        var process = Runtime.getRuntime().exec("docker images");
+        var result = convertStreamToStr(process.getInputStream());
+        var s1 = result.stream().skip(1).filter(s -> s.startsWith("172.16.1.99/postcommit/")).collect(Collectors.toList());
+        var imageIdMap = list.stream()
+                .collect(Collectors.toMap(
+                        Function.identity(),
+                        e -> s1.stream().filter(f -> f.contains(e)).findFirst()
+                                .map(g -> Stream.of(g.split(" "))
+                                        .filter(h -> h.length() > 0).skip(2)
+                                        .findFirst().orElse(""))
+                                .orElse("")));
+        imageIdMap.forEach((name,imageId) -> {
+            try {
+                final var fullTag = "172.16.1.99/transwarp/" + name + ":" + tag;
+                System.out.println("docker tag " + imageId + " " + fullTag);
+                var p = Runtime.getRuntime().exec("docker tag " + imageId + " " + fullTag);
+                p.waitFor();
+                System.out.println("docker push " + fullTag);
+                p = Runtime.getRuntime().exec("docker push " + fullTag);
+                p.waitFor();
+            } catch (IOException | InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        System.out.println("=======");
+        list.forEach(e -> {
+            System.out.println("name :" + e);
+            System.out.println("imageId :" + imageIdMap.get(e));
+            System.out.println(digestMap.get(e));
+            System.out.println("=======");
+        });
+    }
+
     public List<String> convertStreamToStr(InputStream is) {
         final String chars = "\n";
         String result;
@@ -148,26 +200,5 @@ public class DemoTest {
         System.out.println(JsonUtil.obj2PrettyJson(user));
     }
 
-    @Test
-    public void  demo1() {
-        System.out.println("\n\013hello.proto" +
-                "\"\035\n\013GrpcRequest" +
-                "\022\016\n\006params\030\001 \001" +
-                "(\t\"\034\n\tGrpcReply\022\017\n\007" +
-                "message\030\001 \001(\t24\n\007Greeter\022)\n\013" +
-                "sendMessage\022\014.GrpcRequest\032\n" +
-                ".GrpcReply\"\000B\023B\tGrpcProtoP\001\242\002\003HLWb\006proto3");
-    }
 
-    @Test
-    public void demo2() {
-        FileUtil.TextFile file =  FileUtil.readText("/Users/liudi/Downloads/demo.json",false);
-        System.out.println(file);
-    }
-
-    @Test
-    public void demo3() {
-        JsonUtil.json2Obj("{\"abc\":w,\"sds\":null,\"serviceName\":null,\"beanName\":\"ssssrrrr\",\"ssss\":null,\"cronExpression\":null,\"params\":null,\"status\":null,\"host\":null,\"port\":null,\"createTime\":null}",JobBean.class);
-        System.out.println();
-    }
 }
