fd29927bea2259100b77e8b2150b5d8cf6292d5e
liuxiaohei <di.liu@transwarp.io>
2020-10-13 22:14:57 +0800

    aaa

---
+++ luca-webflux/src/test/java/org/ld/leetcode/Solution.java
@@ -0,0 +1,40 @@
+package org.ld.leetcode;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class Solution {
+
+    @Test
+    public void demo() {
+        int[] nums = {3, 2, 3};
+        var target = 6;
+        var a = twoSum(nums, target);
+        Object b = null;
+    }
+
+
+    /**
+     * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+     * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
+     */
+    public int[] twoSum(int[] nums, int target) {
+        int[] result = new int[2];
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if ((nums[i] + nums[j]) == target) {
+                    result[0] = i;
+                    result[1] = j;
+                    return result;
+                }
+            }
+        }
+        return result;
+    }
+
+}
