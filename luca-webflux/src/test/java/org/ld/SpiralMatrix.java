ec0d9f4cf6be49c656dd8f23ffa26cf2d77afaf8
liuxiaohei <di.liu@transwarp.io>
2020-11-26 23:15:08 +0800

    aaa

--- luca-webflux/src/test/java/org/ld/SpiralMatrix.java
+++ luca-webflux/src/test/java/org/ld/SpiralMatrix.java
@@ -1,86 +1,48 @@
 package org.ld;
 
-import java.util.stream.Stream;
+import java.util.stream.IntStream;
 
+/**
+ * 螺旋打印矩阵
+ */
 public class SpiralMatrix {
 
-    enum Direction {
-        right(1,2),
-        down(2,3),
-        left(3,4),
-        up(4,1);
-
-        Direction(Integer value, Integer nextValue) {
-            this.value = value;
-            this.nextValue = nextValue;
+    public int[][] generateMatrix(int m,int n) {
+        int[][] res = new int[m][n];
+        final int total = m * n;
+        int num = 1;
+        int rowBegin = 0;
+        int rowEnd = m - 1;
+        int colBegin = 0;
+        int colEnd = n - 1;
+        while (num <= total) {
+            for (int y = colBegin; y <= colEnd; y++)
+                res[rowBegin][y] = num++;
+            rowBegin++;
+            for (int x = rowBegin; x <= rowEnd; x++)
+                res[x][colEnd] = num++;
+            colEnd--;
+            for (int y = colEnd; y >= colBegin; y--)
+                res[rowEnd][y] = num++;
+            rowEnd--;
+            for (int x = rowEnd; x >= rowBegin; x--)
+                res[x][colBegin] = num++;
+            colBegin++;
         }
-
-        public Integer value;
-        public Integer nextValue;
-
-        public Direction getNextDirection() {
-            return Stream.of(values()).filter(e -> e.nextValue.equals(this.value)).findAny().orElse(Direction.right);
-        }
-    }
-
-    public int[][] createMatrix(int n) {
-        int[][] matrix = new int[n][n];           //n*n的二维数组，初始元素值都为0
-        Direction direction = Direction.right;
-        int numb = n * n;
-        int i = 0, j = 0;
-        for (int p = 1; p <= numb; p++) {
-            matrix[i][j] = p;
-            if (direction.equals(Direction.right)) {
-                if (j + 1 < n && matrix[i][j + 1] == 0) {
-                    j++;
-                } else {
-                    i++;
-                    direction = direction.getNextDirection();
-                    continue;
-                }
-            }
-            if (direction.equals(Direction.down)) {
-                if (i + 1 < n && matrix[i + 1][j] == 0) {
-                    i++;
-                } else {
-                    j--;
-                    direction = direction.getNextDirection();
-                    continue;
-                }
-            }
-            if (direction.equals(Direction.left)) {
-                if (j - 1 >= 0 && matrix[i][j - 1] == 0) {
-                    j--;
-                } else {
-                    i--;
-                    direction = direction.getNextDirection();
-                    continue;
-                }
-            }
-            if (direction.equals(Direction.up)) {
-                if (i - 1 >= 0 && matrix[i - 1][j] == 0) {
-                    i--;
-                } else {
-                    j++;
-                    direction = direction.getNextDirection();
-                }
-            }
-        }
-        return matrix;
+        return res;
     }
 
     /**
      * 测试
      */
     public static void main(String[] args) {
-        int n = 4;
+        int m = 6; // 多少行
+        int n = 4; // 多少列
         SpiralMatrix s = new SpiralMatrix();
-        int[][] data = s.createMatrix(n);
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                System.out.print(data[i][j] + " ");
-            }
-            System.out.println();
-        }
+        int[][] data = s.generateMatrix(m,n);
+        IntStream.range(0,m)
+                .peek(e -> System.out.println())
+                .forEach(i -> IntStream.range(0,n)
+                        .forEach(j -> System.out.print(data[i][j] + "\t")));
     }
 }
