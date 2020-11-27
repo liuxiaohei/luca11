b8f03c8fc6ab34799a171dcf891a89a53dbefea4
liuxiaohei <di.liu@transwarp.io>
2020-11-27 10:11:43 +0800

    aaa

--- luca-webflux/src/test/java/org/ld/SpiralMatrix.java
+++ luca-webflux/src/test/java/org/ld/SpiralMatrix.java
@@ -7,7 +7,7 @@ import java.util.stream.IntStream;
  */
 public class SpiralMatrix {
 
-    public int[][] generateMatrix(int m,int n) {
+    public int[][] generateMatrix(int m, int n) {
         int[][] res = new int[m][n];
         final int total = m * n;
         int num = 1;
@@ -16,16 +16,16 @@ public class SpiralMatrix {
         int colBegin = 0;
         int colEnd = n - 1;
         while (num <= total) {
-            for (int y = colBegin; y <= colEnd; y++)
+            for (int y = colBegin; y <= colEnd && num <= total; y++)
                 res[rowBegin][y] = num++;
             rowBegin++;
-            for (int x = rowBegin; x <= rowEnd; x++)
+            for (int x = rowBegin; x <= rowEnd && num <= total; x++)
                 res[x][colEnd] = num++;
             colEnd--;
-            for (int y = colEnd; y >= colBegin; y--)
+            for (int y = colEnd; y >= colBegin && num <= total; y--)
                 res[rowEnd][y] = num++;
             rowEnd--;
-            for (int x = rowEnd; x >= rowBegin; x--)
+            for (int x = rowEnd; x >= rowBegin && num <= total; x--)
                 res[x][colBegin] = num++;
             colBegin++;
         }
@@ -36,13 +36,13 @@ public class SpiralMatrix {
      * 测试
      */
     public static void main(String[] args) {
-        int m = 6; // 多少行
+        int m = 3; // 多少行
         int n = 4; // 多少列
         SpiralMatrix s = new SpiralMatrix();
-        int[][] data = s.generateMatrix(m,n);
-        IntStream.range(0,m)
+        int[][] data = s.generateMatrix(m, n);
+        IntStream.range(0, m)
                 .peek(e -> System.out.println())
-                .forEach(i -> IntStream.range(0,n)
+                .forEach(i -> IntStream.range(0, n)
                         .forEach(j -> System.out.print(data[i][j] + "\t")));
     }
 }
