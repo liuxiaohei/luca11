c166566e3eeaa502b83a01d7776582cb9d3933b6
liuxiaohei <di.liu@transwarp.io>
2021-03-19 10:58:49 +0800

    aaa

--- luca-base/src/main/java/org/ld/redis/LongRedisTemplate.java
+++ luca-base/src/main/java/org/ld/redis/LongRedisTemplate.java
@@ -8,21 +8,27 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
+import java.util.Optional;
+
 /**
  * 返回long类型的redis模板
  */
 public class LongRedisTemplate extends RedisTemplate<String, Long> {
 
     public LongRedisTemplate(RedisConnectionFactory connectionFactory) {
-        setKeySerializer(new StringRedisSerializer());
-        setValueSerializer(new RedisSerializer<Long>() {
+        var stringSerializer = new StringRedisSerializer();
+        setKeySerializer(stringSerializer);
+        setHashKeySerializer(stringSerializer);
+        var longSerializer = new RedisSerializer<Long>() {
             public Long deserialize(byte[] bytes) {
-                return bytes == null || bytes.length == 0 ? null : Long.valueOf(new String(bytes));
+                return Optional.ofNullable(bytes).map(String::new).map(Long::valueOf).orElse(null);
             }
             public byte[] serialize(Long t) {
                 return t == null ? new byte[0] : t.toString().getBytes();
             }
-        });
+        };
+        setValueSerializer(longSerializer);
+        setHashValueSerializer(longSerializer);
         setConnectionFactory(connectionFactory);
         afterPropertiesSet();
     }
